43dd45d86ae1e324c77ccb8a069eea7d7a7690f4
celyanabella <acely86@gmail.com>
2024-11-16 10:38:44 -0600

    segundo comit

--- proyectoMaven1/src/main/java/cursoAutomatizacionNuevo/proyectoMaven1/App.java
+++ proyectoMaven1/src/main/java/cursoAutomatizacionNuevo/proyectoMaven1/App.java
@@ -6,5 +6,6 @@ package cursoAutomatizacionNuevo.proyectoMaven1;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("egunda llamada");
     }
 }
